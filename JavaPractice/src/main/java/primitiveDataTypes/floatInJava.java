844495428d4fbc006ff004e12324170b9fd58168
palek <palek@192.168.1.36>
2024-12-11 08:53:12 +0530

    Add non-primitive data types, type casting

---
+++ JavaPractice/src/main/java/primitiveDataTypes/floatInJava.java
@@ -0,0 +1,13 @@
+package primitiveDataTypes;
+
+public class floatInJava {
+	
+	public static void main(String[] args) {
+		
+		float f = 123.0664f;
+		
+		System.out.println(f);
+		
+	}
+
+}
---
+++ JavaPractice/src/main/java/primitiveDataTypes/intInJava.java
@@ -0,0 +1,12 @@
+package primitiveDataTypes;
+
+public class intInJava {
+	
+	public static void main(String[] args) {
+		
+		int i = 24324324;
+		
+		System.out.println(i);
+	}
+
+}
